03003eaafa3ba42194c6d495037d6b6b02ae2cd1
David Roldán <deroldan98@gmail.com>
2024-03-10 21:53:29 -0500

    Added additional conten to web app

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import EmailsVisualizer from './components/EmailsVisualizer.vue'
+import Footer from './components/Footer.vue'
+import Header from './components/Header.vue'
 </script>
 
 <template>
-  <header>
-    <div>
-      <EmailsVisualizer msg="GO-Z-V-MAIL" />
-    </div>
-  </header>
+  <Header msg="Go-z-v-mail" />
+  <main class="flex flex-col min-h-[60rem]">
+    <EmailsVisualizer />
+  </main>
+  <Footer />
 </template>
---
+++ web/src/components/Header.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <header class="text-3xl w-full bg-gray-900 p-3 text-white">
+    <a href="" class="cursor-pointer">{{ msg }}</a>
+  </header>
+</template>

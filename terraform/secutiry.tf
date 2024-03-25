762f4c26adaeb4e19e5823694b4c5265518815d9
David Roldán <deroldan98@gmail.com>
2024-03-25 16:52:48 -0500

    Add terraform files

---
+++ terraform/secutiry.tf
@@ -0,0 +1,66 @@
+resource "aws_key_pair" "deployer" {
+  key_name   = "aws_key"
+  public_key = file("/home/david/.ssh/aws_key.pub")
+}
+
+resource "aws_security_group" "main" {
+  egress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    }
+  ]
+  ingress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 22
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 22
+    },
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = "Vue page"
+      from_port        = 5173
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 5173
+    },
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = "Server"
+      from_port        = 3001
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 3001
+    },
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = "Zincsearch"
+      from_port        = 4080
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 4080
+    }
+  ]
+}
